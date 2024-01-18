f31dc6d848eec3f57451815757002f103f95defe
Laboratorio 109 Pc-09 <lab-109-inf-09>
2024-01-17 21:19:44 -0400

    primer commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+
+       String nombre = "Samuel";
+
+       double monto = 475.50;
+
+        double resultado = monto * 0.15;
+
+        System.out.println( "bienvenido " + nombre );
+
+        System.out.println( "el resultado es " + resultado);
+    }
+
+
+    }
+
+
